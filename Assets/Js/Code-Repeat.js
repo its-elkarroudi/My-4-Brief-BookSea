4f6086b81e02d0d1b994be8fd5dec24a9e22d1fe
its-elkarroudi <amineelkarroudi02@gmail.com>
2023-11-03 16:16:43 +0100

    Some changes

--- Assets/Js/Code-Repeat.js
+++ Assets/Js/Code-Repeat.js
@@ -36,3 +36,39 @@ subscribeForm.addEventListener('submit', (e) => {
     e.preventDefault();
     formValidate();
 })
+
+// ============================================================= 
+document.addEventListener("DOMContentLoaded", function () {
+    const categoryContainer = document.querySelector(".categories-container");
+    const categoryWidth = categoryContainer.querySelector(".category").offsetWidth;
+    let currentIndex = 0;
+
+    const nextButton = document.querySelector(".next-button");
+    const prevButton = document.querySelector(".prev-button");
+
+nextButton.addEventListener("click", function () {
+    if (currentIndex < categoryContainer.children.length - 1) {
+        currentIndex++;
+        updateCarousel();
+    } else {
+        currentIndex = 0; 
+        updateCarousel();
+    }
+});
+
+prevButton.addEventListener("click", function () {
+    if (currentIndex > 0) {
+        currentIndex--;
+        updateCarousel();
+    } else {
+        currentIndex = categoryContainer.children.length - 1; 
+        updateCarousel();
+    }
+});
+
+
+    function updateCarousel() {
+        const translateX = -currentIndex * categoryWidth;
+        categoryContainer.style.transform = `translateX(${translateX}px)`;
+    }
+});
